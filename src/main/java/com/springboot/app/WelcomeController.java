434491bad4a139c7cf94cc8689459cf2166ee6ed
nikhil076 <nikhil.arvind7@gmail.com>
2021-10-07 14:03:47 +0530

    Added the hello message wwith get request method

--- src/main/java/com/springboot/app/WelcomeController.java
+++ src/main/java/com/springboot/app/WelcomeController.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 	
-	@GetMapping("/welcome")
+	@GetMapping("/hello")
 	public String welcome() {
-		return "Welcome to spring boot app development";
+		return "Hello World";
 	}
 }
