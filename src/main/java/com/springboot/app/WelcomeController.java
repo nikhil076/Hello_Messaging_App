51e568bc62d7b39e1f29baef64fcdc2facb39bee
nikhil076 <nikhil.arvind7@gmail.com>
2021-10-07 14:00:07 +0530

    Initialized the hello messaging app

---
+++ src/main/java/com/springboot/app/WelcomeController.java
@@ -0,0 +1,19 @@
+package com.springboot.app;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome to spring boot app development";
+	}
+}
